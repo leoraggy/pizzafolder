0860f779aa0897825aeffc6c18c6aa4625992a70
leoraggy <leoragual35@gmail.com>
2025-10-21 10:04:21 -0700

    Committing changes

---
+++ public/js/pizza.js
@@ -0,0 +1,52 @@
+// When the form is submitted, validate
+document.getElementById("pizza-form").onsubmit = () => {
+  clearErrors();
+  // flag variable to determine if form data is valid
+  let isValid = true;
+
+  let fname = document.getElementById("fname").value.trim();
+  let lname = document.getElementById("lname").value.trim();
+  let email = document.getElementById("myEmail").value.trim();
+
+  if (!fname) {
+    isValid = false;
+    document.getElementById("err-fname").style.display = "block";
+  }
+
+  if (!lname) {
+    isValid = false;
+    document.getElementById("err-lname").style.display = "block";
+  }
+
+  if (!email || email.indexOf("@") === -1) {
+    document.getElementById("err-email").style.display = "block";
+    isValid = false;
+  }
+
+  let methodButtons = documents.getElementByName("method").value();
+  let count = 0;
+  for (let methodButton of methodButtons) {
+    if (methodButton.checked) {
+      count++;
+    }
+  }
+  if (count === 0) {
+    document.getElementById("err-method").style.display = "block";
+    isValid = false;
+  }
+
+  let size = document.getElementById("size").value;
+  if (size === "none") {
+    document.getElementById("err-size").style.display = "block";
+    isValid = false;
+  }
+
+  return isValid;
+};
+
+function clearErrors() {
+  let errors = document.getElementsByClassName("error");
+  for (let error of errors) {
+    error.style.display = "none";
+  }
+}
